9221ef5782bbdbeaaed42d8e908febbc14f9862d
Mathis Engelbart <mathis.engelbart@gmail.com>
2023-11-16 11:55:55 +0100

    Improve interop with Alan's chat server (MetaMoQ)

--- examples/chat/client.go
+++ examples/chat/client.go
@@ -55,7 +55,7 @@ func NewClient(p *moqtransport.Peer) (*Client, error) {
 	c.peer.OnAnnouncement(func(s string) error {
 		return nil
 	})
-	c.peer.OnSubscription(func(namespace, trackname string, st *moqtransport.SendTrack) (uint64, time.Duration, error) {
+	c.peer.OnSubscription(func(namespace, _ string, st *moqtransport.SendTrack) (uint64, time.Duration, error) {
 		parts := strings.SplitN(namespace, "/", 2)
 		if len(parts) < 2 {
 			return 0, 0, errors.New("invalid trackname")
@@ -70,20 +70,24 @@ func NewClient(p *moqtransport.Peer) (*Client, error) {
 		c.rooms[id].st = st
 		return c.rooms[id].trackID, 0, nil
 	})
-	go c.peer.Run(context.Background(), false)
+	go func() {
+		if err := c.peer.Run(context.Background(), false); err != nil {
+			log.Fatalf("peer.Run error: %v", err)
+		}
+	}()
 	return c, nil
 }
 
-func (c *Client) handleCatalogDeltas(roomID, username string, catalogTrack *moqtransport.ReceiveTrack) {
+func (c *Client) handleCatalogDeltas(roomID, username string, catalogTrack *moqtransport.ReceiveTrack) error {
 	buf := make([]byte, 64_000)
 	for {
 		n, err := catalogTrack.Read(buf)
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
 		delta, err := parseDelta(string(buf[:n]))
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
 		for _, p := range delta.joined {
 			if p == username {
@@ -91,7 +95,7 @@ func (c *Client) handleCatalogDeltas(roomID, username string, catalogTrack *moqt
 			}
 			t, err := c.peer.Subscribe(fmt.Sprintf("moq-chat/%v", roomID), p)
 			if err != nil {
-				log.Fatal(err)
+				return err
 			}
 			go func(room, user string) {
 				fmt.Printf("%v joined the chat %v\n> ", user, room)
@@ -108,7 +112,7 @@ func (c *Client) handleCatalogDeltas(roomID, username string, catalogTrack *moqt
 	}
 }
 
-func (c *Client) joinRoom(roomID, username string) {
+func (c *Client) joinRoom(roomID, username string) error {
 	c.lock.Lock()
 	defer c.lock.Unlock()
 	c.nextTrackID += 1
@@ -117,23 +121,23 @@ func (c *Client) joinRoom(roomID, username string) {
 		st:      nil,
 		rts:     []*moqtransport.ReceiveTrack{},
 	}
-	if err := c.peer.Announce(fmt.Sprintf("moq-chat/%v/%v", roomID, username)); err != nil {
-		log.Fatal(err)
+	if err := c.peer.Announce(fmt.Sprintf("moq-chat/%v/participant/%v", roomID, username)); err != nil {
+		return err
 	}
-	catalogTrack, err := c.peer.Subscribe(fmt.Sprintf("moq-chat/%v", roomID), "")
+	catalogTrack, err := c.peer.Subscribe(fmt.Sprintf("moq-chat/%v", roomID), "/catalog")
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	buf := make([]byte, 64_000)
 	var n int
 	n, err = catalogTrack.Read(buf)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	var participants *chatalog
 	participants, err = parseChatalog(string(buf[:n]))
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	for p := range participants.participants {
 		if p == username {
@@ -141,29 +145,30 @@ func (c *Client) joinRoom(roomID, username string) {
 		}
 		t, err := c.peer.Subscribe(fmt.Sprintf("moq-chat/%v", roomID), p)
 		if err != nil {
-			log.Fatal(err)
+			log.Fatalf("failed to subscribe to participant track: %v", err)
 		}
 		go func(room, user string) {
 			for {
 				buf := make([]byte, 64_000)
 				n, err = t.Read(buf)
 				if err != nil {
-					log.Fatal(err)
+					log.Fatalf("failed to read from participant track: %v", err)
 				}
 				fmt.Fprintf(os.Stdout, "room %v|user %v: %v\n> ", room, user, string(buf[:n]))
 			}
 		}(roomID, p)
 	}
 	go c.handleCatalogDeltas(roomID, username, catalogTrack)
+	return nil
 }
 
-func (c *Client) Run() {
+func (c *Client) Run() error {
 	r := bufio.NewReader(os.Stdin)
 	for {
 		fmt.Fprintf(os.Stdout, "> ")
 		cmd, err := r.ReadString('\n')
 		if err != nil {
-			log.Fatal(err)
+			return err
 		}
 		if strings.HasPrefix(cmd, "join") {
 			fields := strings.Fields(cmd)
@@ -171,7 +176,9 @@ func (c *Client) Run() {
 				fmt.Println("invalid join command, usage: 'join <room id> <username>'")
 				continue
 			}
-			c.joinRoom(fields[1], fields[2])
+			if err = c.joinRoom(fields[1], fields[2]); err != nil {
+				return err
+			}
 			continue
 		}
 		if strings.HasPrefix(cmd, "msg") {
@@ -187,7 +194,7 @@ func (c *Client) Run() {
 			}
 			_, err = c.rooms[fields[1]].st.Write([]byte(strings.TrimSpace(msg)))
 			if err != nil {
-				log.Fatal(err)
+				return fmt.Errorf("failed to write to room: %v", err)
 			}
 			continue
 		}
