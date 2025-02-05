e3fe3df90c051f89beb2b5da45d5081bfe7b766e
Mathis Engelbart <mathis.engelbart@gmail.com>
2025-02-05 22:14:03 +0100

    Remove unused forwarding preference constants

--- object.go
+++ object.go
@@ -1,13 +1,5 @@
 package moqtransport
 
-type ObjectForwardingPreference int
-
-const (
-	ObjectForwardingPreferenceNone ObjectForwardingPreference = iota
-	ObjectForwardingPreferenceDatagram
-	ObjectForwardingPreferenceSubgroup
-)
-
 type Object struct {
 	GroupID    uint64
 	SubGroupID uint64
