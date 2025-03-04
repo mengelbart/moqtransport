ea3de73ab025c291f52e3f8ccb53bf212250dfb5
Mathis Engelbart <mathis.engelbart@gmail.com>
2025-03-04 22:10:23 +0100

    Set log level via environment variable

--- logging.go
+++ logging.go
@@ -2,15 +2,43 @@ package moqtransport
 
 import (
 	"log/slog"
+	"math"
 	"os"
+	"strings"
 )
+
+const logEnv = "MOQ_LOG_LEVEL"
+
+const (
+	LogLevelNone = math.MaxInt
+)
+
+var moqtransportLogLevel = new(slog.LevelVar)
+
+var defaultLogger *slog.Logger
 
 func init() {
 	h := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
 		AddSource: false,
-		Level:     slog.LevelDebug,
+		Level:     moqtransportLogLevel,
 	})
+	moqtransportLogLevel.Set(readLoggingEnv())
 	defaultLogger = slog.New(h)
 }
 
-var defaultLogger *slog.Logger
+func readLoggingEnv() slog.Level {
+	switch strings.ToLower(os.Getenv(logEnv)) {
+	case "":
+		return LogLevelNone
+	case "debug":
+		return slog.LevelDebug
+	case "info":
+		return slog.LevelInfo
+	case "warn":
+		return slog.LevelWarn
+	case "error":
+		return slog.LevelError
+	default:
+		return LogLevelNone
+	}
+}
