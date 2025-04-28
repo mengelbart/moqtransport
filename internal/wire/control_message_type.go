1c9bbc1b9a08c26fc0e233dc16c11e91417245d2
Mathis Engelbart <mathis.engelbart@gmail.com>
2025-04-28 14:47:46 +0200

    Update control message IDs and reorder types

--- internal/wire/control_message_type.go
+++ internal/wire/control_message_type.go
@@ -9,44 +9,83 @@ type controlMessageType uint64
 
 // Control message types
 const (
-	messageTypeSubscribeUpdate         controlMessageType = 0x02
-	messageTypeSubscribe               controlMessageType = 0x03
-	messageTypeSubscribeOk             controlMessageType = 0x04
-	messageTypeSubscribeError          controlMessageType = 0x05
-	messageTypeAnnounce                controlMessageType = 0x06
-	messageTypeAnnounceOk              controlMessageType = 0x07
-	messageTypeAnnounceError           controlMessageType = 0x08
-	messageTypeUnannounce              controlMessageType = 0x09
-	messageTypeUnsubscribe             controlMessageType = 0x0a
-	messageTypeSubscribeDone           controlMessageType = 0x0b
-	messageTypeAnnounceCancel          controlMessageType = 0x0c
-	messageTypeTrackStatusRequest      controlMessageType = 0x0d
-	messageTypeTrackStatus             controlMessageType = 0x0e
-	messageTypeGoAway                  controlMessageType = 0x10
+	messageTypeClientSetup controlMessageType = 0x20
+	messageTypeServerSetup controlMessageType = 0x21
+
+	messageTypeGoAway controlMessageType = 0x10
+
+	messageTypeMaxRequestID    controlMessageType = 0x15
+	messageTypeRequestsBlocked controlMessageType = 0x1a
+
+	messageTypeSubscribe       controlMessageType = 0x03
+	messageTypeSubscribeOk     controlMessageType = 0x04
+	messageTypeSubscribeError  controlMessageType = 0x05
+	messageTypeUnsubscribe     controlMessageType = 0x0a
+	messageTypeSubscribeUpdate controlMessageType = 0x02
+	messageTypeSubscribeDone   controlMessageType = 0x0b
+
+	messageTypeFetch       controlMessageType = 0x16
+	messageTypeFetchOk     controlMessageType = 0x18
+	messageTypeFetchError  controlMessageType = 0x19
+	messageTypeFetchCancel controlMessageType = 0x17
+
+	messageTypeTrackStatusRequest controlMessageType = 0x0d
+	messageTypeTrackStatus        controlMessageType = 0x0e
+
+	messageTypeAnnounce       controlMessageType = 0x06
+	messageTypeAnnounceOk     controlMessageType = 0x07
+	messageTypeAnnounceError  controlMessageType = 0x08
+	messageTypeUnannounce     controlMessageType = 0x09
+	messageTypeAnnounceCancel controlMessageType = 0x0c
+
 	messageTypeSubscribeAnnounces      controlMessageType = 0x11
 	messageTypeSubscribeAnnouncesOk    controlMessageType = 0x12
 	messageTypeSubscribeAnnouncesError controlMessageType = 0x13
 	messageTypeUnsubscribeAnnounces    controlMessageType = 0x14
-	messageTypeMaxSubscribeID          controlMessageType = 0x15
-	messageTypeFetch                   controlMessageType = 0x16
-	messageTypeFetchCancel             controlMessageType = 0x17
-	messageTypeFetchOk                 controlMessageType = 0x18
-	messageTypeFetchError              controlMessageType = 0x19
-	messageTypeSubscribesBlocked       controlMessageType = 0x1a
-	messageTypeClientSetup             controlMessageType = 0x40
-	messageTypeServerSetup             controlMessageType = 0x41
 )
 
 func (mt controlMessageType) String() string {
 	switch mt {
-	case messageTypeSubscribeUpdate:
-		return "SubscribeUpdateMessage"
+	case messageTypeClientSetup:
+		return "ClientSetupMessage"
+	case messageTypeServerSetup:
+		return "ServerSetupMessage"
+
+	case messageTypeGoAway:
+		return "GoAwayMessage"
+
+	case messageTypeMaxRequestID:
+		return "MaxSubscribeIDMessage"
+	case messageTypeRequestsBlocked:
+		return "SubscribesBlockedMessage"
+
 	case messageTypeSubscribe:
 		return "SubscribeMessage"
 	case messageTypeSubscribeOk:
 		return "SubscribeOkMessage"
 	case messageTypeSubscribeError:
 		return "SubscribeErrorMessage"
+	case messageTypeUnsubscribe:
+		return "UnsubscribeMessage"
+	case messageTypeSubscribeUpdate:
+		return "SubscribeUpdateMessage"
+	case messageTypeSubscribeDone:
+		return "SubscribeDoneMessage"
+
+	case messageTypeFetch:
+		return "FetchMessage"
+	case messageTypeFetchOk:
+		return "FetchOkMessage"
+	case messageTypeFetchError:
+		return "FetchErrorMessage"
+	case messageTypeFetchCancel:
+		return "FetchCancelMessage"
+
+	case messageTypeTrackStatusRequest:
+		return "TrackStatusRequestMessage"
+	case messageTypeTrackStatus:
+		return "TrackStatusMessage"
+
 	case messageTypeAnnounce:
 		return "AnnounceMessage"
 	case messageTypeAnnounceOk:
@@ -55,40 +94,15 @@ func (mt controlMessageType) String() string {
 		return "AnnounceErrorMessage"
 	case messageTypeUnannounce:
 		return "AnannounceMessage"
-	case messageTypeUnsubscribe:
-		return "UnsubscribeMessage"
-	case messageTypeSubscribeDone:
-		return "SubscribeDoneMessage"
 	case messageTypeAnnounceCancel:
 		return "AnnounceCancelMessage"
-	case messageTypeTrackStatusRequest:
-		return "TrackStatusRequestMessage"
-	case messageTypeTrackStatus:
-		return "TrackStatusMessage"
-	case messageTypeGoAway:
-		return "GoAwayMessage"
-	case messageTypeClientSetup:
-		return "ClientSetupMessage"
-	case messageTypeServerSetup:
-		return "ServerSetupMessage"
-	case messageTypeFetch:
-		return "FetchMessage"
-	case messageTypeFetchCancel:
-		return "FetchCancelMessage"
-	case messageTypeFetchError:
-		return "FetchErrorMessage"
-	case messageTypeFetchOk:
-		return "FetchOkMessage"
-	case messageTypeMaxSubscribeID:
-		return "MaxSubscribeIDMessage"
+
 	case messageTypeSubscribeAnnounces:
 		return "SubscribeAnnouncesMessage"
-	case messageTypeSubscribeAnnouncesError:
-		return "SubscribeAnnouncesErrorMessage"
 	case messageTypeSubscribeAnnouncesOk:
 		return "SubscribeAnnouncesOkMessage"
-	case messageTypeSubscribesBlocked:
-		return "SubscribesBlockedMessage"
+	case messageTypeSubscribeAnnouncesError:
+		return "SubscribeAnnouncesErrorMessage"
 	case messageTypeUnsubscribeAnnounces:
 		return "UnsubscribeAnnouncesMessage"
 	}
