12465506970490fe56294c840722059f70f75e96
Mathis Engelbart <mathis.engelbart@gmail.com>
2025-05-07 08:19:37 +0200

    Move queue to separate file

--- session.go
+++ session.go
@@ -37,42 +37,6 @@ type objectMessageParser interface {
 	Messages() iter.Seq2[*wire.ObjectMessage, error]
 }
 
-type queue[T any] struct {
-	queue chan T
-}
-
-func newQueue[T any]() *queue[T] {
-	return &queue[T]{
-		queue: make(chan T, 1),
-	}
-}
-
-// For some reason the linter thinks this is unused, but it implements the
-// controlMessageQueue interface.
-//
-//nolint:unused
-func (q *queue[T]) enqueue(ctx context.Context, msg T) error {
-	select {
-	case q.queue <- msg:
-		return nil
-	case <-ctx.Done():
-		return context.Cause(ctx)
-	}
-}
-
-// For some reason the linter thinks this is unused, but it implements the
-// controlMessageQueue interface.
-//
-//nolint:unused
-func (q *queue[T]) dequeue(ctx context.Context) (T, error) {
-	select {
-	case <-ctx.Done():
-		return *new(T), context.Cause(ctx)
-	case msg := <-q.queue:
-		return msg, nil
-	}
-}
-
 // A Session is an endpoint of a MoQ Session session.
 type Session struct {
 	Protocol    Protocol
