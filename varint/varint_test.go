06841cd246179bc247c587a09a5cd8e907977e13
Mathis Engelbart <mathis.engelbart@gmail.com>
2026-07-22 14:38:10 +0200

    Add new varint encoding package

---
+++ varint/varint_test.go
@@ -0,0 +1,62 @@
+package varint
+
+import (
+	"fmt"
+	"io"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseVarint(t *testing.T) {
+	cases := []struct {
+		bytes []byte
+		value uint64
+		count int
+		err   error
+	}{
+		{[]byte{}, 0, 0, io.EOF},
+		{[]byte{0x00}, 0, 1, nil},
+		{[]byte{0x01}, 1, 1, nil},
+		{[]byte{0x7F}, 127, 1, nil},
+		{[]byte{0x25}, 37, 1, nil},
+		{[]byte{0x80, 0x25}, 37, 2, nil},
+		{[]byte{0x80, 0x00}, 0, 2, nil},
+		{[]byte{0xED, 0x7F, 0x3E, 0x7D}, 226_442_877, 4, nil},
+		{[]byte{0xFA, 0xA1, 0xA0, 0xE4, 0x03, 0xD8}, 2_893_212_287_960, 6, nil},
+		{[]byte{0xFC, 0x89, 0x98, 0xAB, 0xC6, 0x6B, 0xC0}, 151_288_809_941_952, 7, nil},
+		{[]byte{0xFE, 0xFA, 0x31, 0x8F, 0xA8, 0xE3, 0xCA, 0x11}, 70_423_237_261_249_041, 8, nil},
+		{[]byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}, 18_446_744_073_709_551_615, 9, nil},
+	}
+	for _, c := range cases {
+		t.Run(fmt.Sprintf("%v", c.bytes), func(t *testing.T) {
+			value, bytes, err := Parse(c.bytes)
+			assert.Equal(t, c.value, value, "Parse(%v) = %d, want %d", c.bytes, value, c.value)
+			assert.Equal(t, c.count, bytes, "Parse(%v) = %d bytes, want %d", c.bytes, bytes, c.count)
+			assert.Equal(t, c.err, err, "Parse(%v) = %v, want %v", c.bytes, err, c.err)
+		})
+	}
+}
+
+func TestAppendVarint(t *testing.T) {
+	cases := []struct {
+		value uint64
+		bytes []byte
+	}{
+		{0, []byte{0x00}},
+		{1, []byte{0x01}},
+		{37, []byte{0x25}},
+		{127, []byte{0x7F}},
+		{226_442_877, []byte{0xED, 0x7F, 0x3E, 0x7D}},
+		{2_893_212_287_960, []byte{0xFA, 0xA1, 0xA0, 0xE4, 0x03, 0xD8}},
+		{151_288_809_941_952, []byte{0xFC, 0x89, 0x98, 0xAB, 0xC6, 0x6B, 0xC0}},
+		{70_423_237_261_249_041, []byte{0xFE, 0xFA, 0x31, 0x8F, 0xA8, 0xE3, 0xCA, 0x11}},
+		{18_446_744_073_709_551_615, []byte{0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF}},
+	}
+	for _, c := range cases {
+		t.Run(fmt.Sprintf("%d", c.value), func(t *testing.T) {
+			bytes := Append([]byte{}, c.value)
+			assert.Equal(t, c.bytes, bytes, "Append(%d) = %v, want %v", c.value, bytes, c.bytes)
+		})
+	}
+}
