7fae1a3ec167d70078be1c5bb998cf7ad31a1ee7
Mathis Engelbart <mathis.engelbart@gmail.com>
2026-07-28 11:34:07 +0200

    Fix some linter issues

--- session_helpers.go
+++ session_helpers.go
@@ -1,30 +1,10 @@
 package moqtransport
 
 import (
-	"encoding/binary"
-	"errors"
-	"math"
 	"slices"
 
 	"github.com/mengelbart/moqtransport/internal/wire"
-	"github.com/quic-go/quic-go/quicvarint"
 )
-
-var errControlMessageTooLarge = errors.New("control message too large")
-
-func compileMessage(msg wire.ControlMessage) ([]byte, error) {
-	buf := make([]byte, 0, 4096)
-	buf = quicvarint.Append(buf, uint64(msg.Type()))
-	tl := len(buf)
-	buf = append(buf, 0x00, 0x00) // length placeholder
-	buf = msg.Append(buf)
-	length := len(buf[tl+2:])
-	if length > math.MaxUint16 {
-		return nil, errControlMessageTooLarge
-	}
-	binary.BigEndian.PutUint16(buf[tl:tl+2], uint16(length))
-	return buf, nil
-}
 
 func validatePathParameter(setupParameters wire.KVPList, protocolIsQUIC bool) (string, error) {
 	index := slices.IndexFunc(setupParameters, func(p wire.KeyValuePair) bool {
